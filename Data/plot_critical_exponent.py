e16618ce26969353412402c777e3857e30d64574
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-06-01 13:08:39 +0200

    more Data

--- Data/plot_critical_exponent.py
+++ Data/plot_critical_exponent.py
@@ -6,14 +6,16 @@ from scipy.optimize import curve_fit
 data = defaultdict(list)
 max_steps = -1
 
-with open("critical_exponent_data.txt", "r") as file:
-    for line in file:
-        steps, distance = line.split()
-        if int(steps) > max_steps:
-            max_steps = int(steps)
-        data[int(steps)].append(float(distance))
+for suffix in ("", "1", "2", "3", "4", "5"):
+    with open("critical_exponent_data" + suffix + ".txt", "r") as file:
+        for line in file:
+            steps, distance = line.split()
+            if int(steps) > max_steps:
+                max_steps = int(steps)
+            data[int(steps)].append(float(distance))
 
-x = np.linspace(0, max_steps, max_steps + 1)
+#x = np.linspace(0, max_steps, max_steps + 1)
+x = np.array(sorted(data.keys()))
 y = np.array([np.mean(data[i]) for i in x])
 s_y = np.array([np.std(data[i]) / len(data[i])**.5 for i in x])
 
@@ -25,7 +27,7 @@ print(results)
 
 chi2 = (sum((y[5:] - f(x, *results[0])[5:])**2 / s_y[5:]**2) / len(y[5:]))
 print(chi2)
-plt.plot(x, y, "xr")
-plt.errorbar(x, y, yerr=s_y, color="red")
-plt.plot(x, f(x, *results[0]), "--b")
+#plt.plot(x, y, "xr")
+plt.errorbar(x, y, yerr=s_y, fmt="xr", barsabove=True)
+plt.plot(x, f(x, *results[0]), "--g", lw=2)
 plt.show()

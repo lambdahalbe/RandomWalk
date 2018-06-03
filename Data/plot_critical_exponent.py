79f972a4278a39fffd13d46295fe3c05112416b3
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-06-03 22:35:12 +0200

    plot changed

--- Data/plot_critical_exponent.py
+++ Data/plot_critical_exponent.py
@@ -6,14 +6,14 @@ from scipy.optimize import curve_fit
 data = defaultdict(list)
 max_steps = -1
 
-for suffix in ("", "1", "2", "3", "4", "5"):
-    with open("critical_exponent_data" + suffix + ".txt", "r") as file:
-        for line in file:
-            steps, distance = line.split()
-            if int(steps) > max_steps:
-                max_steps = int(steps)
-            data[int(steps)].append(float(distance))
-
+with open("self_avoiding_triangular_data.txt", "r") as file:
+    for line in file:
+        print(line)
+        steps, distance = line.split()
+        if int(steps) > max_steps:
+            max_steps = int(steps)
+        data[int(steps)].append(float(distance))
+print(data)
 #x = np.linspace(0, max_steps, max_steps + 1)
 x = np.array(sorted(data.keys()))
 y = np.array([np.mean(data[i]) for i in x])
@@ -21,13 +21,14 @@ s_y = np.array([np.std(data[i]) / len(data[i])**.5 for i in x])
 
 def f(x, a, gamma):
     return a * x**gamma
-
+"""
 results = curve_fit(f, x[5:], y[5:], p0=[1, .5], sigma=s_y[5:])
 print(results)
 
 chi2 = (sum((y[5:] - f(x, *results[0])[5:])**2 / s_y[5:]**2) / len(y[5:]))
 print(chi2)
-#plt.plot(x, y, "xr")
-plt.errorbar(x, y, yerr=s_y, fmt="xr", barsabove=True)
-plt.plot(x, f(x, *results[0]), "--g", lw=2)
+"""
+plt.plot(x, y, "xr")
+#plt.errorbar(x, y, yerr=s_y, fmt="xr", barsabove=True)
+#plt.plot(x, f(x, *results[0]), "--g", lw=2)
 plt.show()

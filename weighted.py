9b63138525598cb03c19096ba96ce0ff28095be1
FranziskaMoos <franziska.moos@web.de>
2018-06-13 15:37:47 +0200

    Data taking with Weights

---
+++ weighted.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 13 12:33:20 2018
+
+@author: franz
+"""
+
+from walker import triangular_walker
+import numpy as np
+import sys
+
+#if len(sys.argv) < 2:
+#    exit()
+
+length = 0
+runs_per_length = 200
+
+
+weights = []
+with open("test.txt", "w") as file:
+    while length < 50:
+        count = 0
+        while count < runs_per_length:
+            walker = triangular_walker(np.zeros((2)), self_avoiding=True)
+            try:
+                for foo in range(length):
+                    walker.walk()
+            except IndexError:          
+                continue
+                
+            file.write(str(length) + " " + str(np.linalg.norm(walker.position_coordinates(walker.pos))) + " " + str(walker.W) + "\n")      
+            count += 1
+        length += 1
+        

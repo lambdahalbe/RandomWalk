6eb8ad1f2755994052b8d2e60f73d76eeb016b72
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-06-13 13:46:28 +0200

    worked on perm

--- perm.py
+++ perm.py
@@ -1,17 +1,25 @@
+from random import choice
+
 
 
 class perm:
 
 
-    def __init__(self, walker, max_length):
+    def __init__(self, walker, max_length, end_weight):
         self.Weights = []  # Weights at length N
         self.Z = []  # grand canonical partion sum
         self.Copys = []
         for i in range(max_length + 1):
             self.Copys.append(0)
-        self.Copys[0] = 1
+            self.Z.append(0)
+            self.Weights.append(0)
+        self.Copys[0] = 0
+        self.Z[0] = 1
         self.max_length = max_length
         self.walker = walker
+        self.c_lower = .3  # TODO find better values
+        self.c_upper = 3
+        self.end_weight = end_weight
 
 
     def init_partition_sum(self):
@@ -28,23 +36,44 @@ class perm:
                 continue
 
     def step(self):
-        if self.walker.steps == self.max_length or len(self.walker.atmosphere) == 0:
+        if self.walker.steps == self.max_length or self.walker.atmosphere() == 0:
+            print("Reached max_length or dead end")
             self.Copys[self.walker.steps] = 0
         else:
-            pass  # TODO pruning/enrichment
+            W_upper = self.c_upper * self.Z[self.walker.steps] / self.Z[0]
+            W_lower = self.c_lower * self.Z[self.walker.steps] / self.Z[0]
+            if self.walker.W > W_upper:
+                print("Enriched")
+                self.Copys[self.walker.steps] += 2
+                self.walker.W /= 2
+                self.Weights[self.walker.stpes] = self.walker.W
+            elif self.walker.W < W_lower:
+                print("Prune attempt")
+                self.walker.W *= 2
+                self.Weights[self.walker.steps] = self.walker.W
+                if choice([True, False]):
+                    print("Pruning accepted")
+                    self.Copys[self.walker.steps] = 0
+
         
         if self.Copys[self.walker.steps] == 0:
             while self.walker.steps > 0  and self.Copys[self.walker.steps] == 0:
+                print("Shrinking to length " + str(self.walker.steps))
                 self.walker.back_propagate()
                 self.walker.W = self.Weights[self.walker.steps]
 
         if self.walker.steps == 0 and self.Copys[self.walker.steps] == 0:
+            print("Start new run")
             self.Copys[0] = 1
             self.walker.W = 1
-        else:
-            if self.walker.atmosphere() > 0:
+            self.Z[0] += self.walker.W
+        elif self.walker.atmosphere() > 0:
+                print("Step at length " + str(self.walker.steps))
                 self.Copys[self.walker.steps] -= 1
                 self.walker.walk()
-                self.Weights[self.walker.steps] =self.walker.W
+                self.Weights[self.walker.steps] = self.walker.W
                 self.Z[self.walker.steps] += self.walker.W
 
+    def run(self):
+        while self.Z[-1] < self.end_weight:
+            self.step()
--- perm_test.py
+++ perm_test.py
@@ -3,6 +3,5 @@ from walker import hexagonal_walker, triangular_walker, walker
 import numpy as np
 
 
-P = perm(triangular_walker(np.zeros((2)), self_avoiding = True), 500)
-P.init_partition_sum()
-print(P.Z)
+P = perm(triangular_walker(np.zeros((2)), self_avoiding = True), 10, 10)
+P.run()

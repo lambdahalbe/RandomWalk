ee115a3c6f13718423fe3b8a9ae5c391d438d440
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-06-27 23:04:38 +0200

    improvements on perm

--- perm.py
+++ perm.py
@@ -5,22 +5,25 @@ from random import choice
 class perm:
 
 
-    def __init__(self, walker, max_length, end_weight, filename):
-        self.Weights = []  # Weights at length N
-        self.Z = []  # grand canonical partion sum
-        self.Copys = []
-        for i in range(max_length + 1):
-            self.Copys.append(0)
-            self.Z.append(0)
-            self.Weights.append(0)
-        self.Z[0] = 1
+    def __init__(self, walker, max_length, run_threshold, filename, metadata_filename=None):
+        self.Weights = [0] * (max_length + 1)  # Weights at Length N
+        self.Z = [0] * (max_length + 1)        # Grand Canonical Partion Sum
+        self.Copys = [0] * (max_length + 1)    # Number of Copys per Length
+        self.dppl = [0] * (max_length + 1)     # DataPoints Per Length
+        self.Z[0] = 1                          # Initilize to avoid division by 0
         self.max_length = max_length
         self.walker = walker
         self.c_lower = .3  # TODO find better values
-        self.c_upper = 2.5
-        self.end_weight = end_weight
+        self.c_upper = 3
+        self.run_threshold = run_threshold
+        self.filename = filename
         self.file = open(filename, "w")
-        self.n = 0
+        self.successful_runs = 0
+        self.running = False
+        self.meta = False
+        if metadata_filename:
+            self.meta = True
+            self.metadata_file = open(metadata_filename, "w")
 
 
     def init_partition_sum(self):
@@ -40,8 +43,10 @@ class perm:
         if self.walker.steps == self.max_length or self.walker.atmosphere() == 0:
             self.Copys[self.walker.steps] = 0
             if self.walker.steps == self.max_length:
-                self.n += 1
-                print("MaxLength reached", self.n)
+                self.successful_runs += 1
+                print("MaxLength reached", self.successful_runs)
+                if self.successful_runs >= self.run_threshold:
+                    self.running = False
         else:
             W_upper = self.c_upper * self.Z[self.walker.steps] / self.Z[0]
             W_lower = self.c_lower * self.Z[self.walker.steps] / self.Z[0]
@@ -77,21 +82,34 @@ class perm:
             self.Copys[0] = 1
             self.walker.W = 1
             self.Z[0] += self.walker.W
+            self.dppl[0] += 1
             self.writedata()
         
         if self.walker.atmosphere() > 0:
                 self.Copys[self.walker.steps] -= 1
                 self.walker.walk()
+                self.dppl[self.walker.steps] += 1
                 self.Weights[self.walker.steps] = self.walker.W
                 self.Z[self.walker.steps] += self.walker.W
                 self.writedata()
 
     def run(self):
-        while self.Z[-1] < self.end_weight:
+        self.running = True
+        while self.running:
             self.step()
         self.file.close()
+        if self.meta:
+            self.writemeta()
+            self.metadata_file.close()
 
     def writedata(self):
         line = str(self.walker.steps) + " " + str(self.walker.end_to_end_distance()) +\
                 " " + str(self.walker.W) + "\n"
         self.file.write(line)
+
+    def writemeta(self):
+        text = "Output Filename: " + self.filename +\
+                "\nWalker: " + str(self.walker) + "\nScaling Factor: " + str(self.walker.scaling_factor) +\
+                "\nMaxLength: " + str(self.max_length) + "\nRunThreshold: " + str(self.run_threshold) +\
+                "\nDatapoints per Length: " + str(self.dppl)
+        self.metadata_file.write(text)
--- perm_test.py
+++ perm_test.py
@@ -3,13 +3,6 @@ from walker import hexagonal_walker, triangular_walker, walker
 import numpy as np
 
 
-P = perm(triangular_walker(np.zeros((2), dtype=int), self_avoiding = True), 500, 1000000000, "blub.txt")
+P = perm(triangular_walker(np.zeros((2), dtype=int), self_avoiding = True, scaling_factor = 2), 800, 8000, "triangular_data.txt", "triangular_meta.txt")
 
-P.init_partition_sum()
-P.walker.scaling_factor = P.Z[-1]**(1/(len(P.Z)-1))
-for i in range(len(P.Z)):
-    P.Z[i] /= P.walker.scaling_factor**i
-print(len(P.Z))
-print(P.Z)
-print(P.walker.scaling_factor)
 P.run()

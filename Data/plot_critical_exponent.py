801cc9e03678be7e2ee76714d08e81151676913a
Franziska Moos <franziska.moos@web.de>
2018-07-02 14:22:18 +0200

    Plot schöner

--- Data/plot_critical_exponent.py
+++ Data/plot_critical_exponent.py
@@ -2,11 +2,12 @@ import matplotlib.pylab as plt
 import numpy as np
 from collections import defaultdict
 from scipy.optimize import curve_fit
+from matplotlib.lines import Line2D
 
 data = defaultdict(list)
 max_steps = -1
 
-with open("self_avoiding_triangular_data.txt", "r") as file:
+with open("critital_exponent_hexagonal_notselfavoiding_1000.txt", "r") as file:
     for line in file:
         steps, distance = line.split()
         if int(steps) > max_steps:
@@ -21,11 +22,14 @@ s_y = np.array([np.std(data[i], ddof=1) / len(data[i])**.5 for i in x])
 def f(x, a, gamma):
     return a * x**gamma
 
-results = curve_fit(f, x[150:], y[150:], p0=[1, .5], sigma=s_y[150:])
-print(results)
+results, error = curve_fit(f, x[150:], y[150:], p0=[1, .5], sigma=s_y[150:])
 
-chi2 = (sum((y[150:] - f(x, *results[0])[150:])**2 / s_y[150:]**2) / len(y[150:]))
-print(chi2)
+err = np.sqrt(np.diag(error))
+
+chi2 = (sum((y[150:] - f(x, *results)[150:])**2 / s_y[150:]**2) / len(y[150:]))
+
+
+
 
 # Plot hübsch machen
 fig = plt.figure(figsize=(8,5))                                                            
@@ -33,10 +37,29 @@ ax = fig.add_subplot(1,1,1)
 ax.set_yscale('log')
 ax.set_xscale('log')
 
+#Achsen verschönern
+ticklabels = ax.get_xticklabels() + ax.get_yticklabels()
+for label in ticklabels:
+    label.set_fontsize(15)
+ax.grid(which='both')
+ax.grid(which='minor', alpha=0.2)                                                
+ax.grid(which='major', alpha=0.8)                                      
+plt.xlabel("Steps", fontsize = 15)
+plt.ylabel(r"Average Distance", fontsize = 15)
+
+def ezlegend(*dat, **args):
+    dat = list(dat)
+    for i, line in enumerate(dat):
+        if not str(type(line)).startswith("<class 'matplotlib"):
+            dat[i] = Line2D([],[], ls = "", label = line)
+    plt.legend(**args, handles = dat, fontsize = 15)
 
 
-
-plt.plot(x, y, "xr")
+#Sachen plotten
+line1, = plt.plot(x, y, "xr", label = "Data")
 plt.errorbar(x, y, yerr=s_y, fmt="o", barsabove=True)
-plt.plot(x, f(x, *results[0]), "--g", lw=2)
+line2, = plt.plot(x, f(x, *results), "--g", lw=2, label = r"Fit: d$_n$ = a $\cdot$ n$^\nu$")
+ezlegend(line1, line2, "a = {}$\pm${}".format(round(results[0],2),round(err[0],2)), r"$\nu$ = {}$\pm${}".format(round(results[1],3),round(err[1],3)))
+
+#plt.savefig("test.pdf")
 plt.show()

287fdfc48f3898d833bcc159d07cbb36ab4a1940
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-07-04 17:27:10 +0200

    minor changes

--- display_test.py
+++ display_test.py
@@ -2,5 +2,5 @@ from walker import walker, hexagonal_walker, triangular_walker
 from display import screen
 import numpy as np
 
-walkers = [triangular_walker(np.array([30, 20]), self_avoiding=True) for i in range(1)]
-new_screen = screen(2000, 2000, walkers, update_steps = 1, scale=30, sleep_time=.3)
+walkers = [triangular_walker(np.array([30, 10]), self_avoiding=False) for i in range(500)]
+new_screen = screen(2000, 2000, walkers, update_steps = 1, scale=30, sleep_time=.0)

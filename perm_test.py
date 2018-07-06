1d6a1b59588fb8a07c08617430ca59e259cb80f3
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-07-06 13:44:21 +0200

    corrected test.py

--- perm_test.py
+++ perm_test.py
@@ -3,10 +3,6 @@ from walker import hexagonal_walker, triangular_walker, walker
 import numpy as np
 
 
-<<<<<<< HEAD
-P = perm(triangular_walker(np.zeros((2), dtype=int), self_avoiding = True, scaling_factor = 2), 1000, 10000, metadata_filename="triangular_meta.txt")
-=======
 P = perm(triangular_walker(np.zeros((2), dtype=int), self_avoiding = True, scaling_factor = 1.85), 5000, 100000, metadata_filename="triangular_meta.txt")
->>>>>>> 563cdac863c012d1cbb962cd01822a182f23af13
 
 P.run()

147ca8cfef27fee30fac2d7cf0ca9d73d77676b8
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-07-12 16:18:36 +0200

    long video

--- perm_display_test.py
+++ perm_display_test.py
@@ -3,5 +3,10 @@ from perm import *
 from walker import *
 import numpy as np
 
-ps = perm_screen(1000, 1000, perm(hexagonal_walker(np.array([26, 24]), self_avoiding=True, scaling_factor=4.2),100, 10000), update_steps=1, scale = 20, sleep_time=.0, save = True)
+
+p = perm(triangular_walker(np.array([165, 97]), self_avoiding=True, scaling_factor=1.85),1000, 10000)
+for i in range(len(p.Z)):
+    p.Z[i] = 1
+
+ps = perm_screen(1500, 1500, p, update_steps=30, scale = 5, sleep_time=.0, save = True)
 

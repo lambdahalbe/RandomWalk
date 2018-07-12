b618757c8028a8e1bca55bcba7957851ea51b6a6
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-07-12 15:44:32 +0200

    movies

--- perm_display_test.py
+++ perm_display_test.py
@@ -3,5 +3,5 @@ from perm import *
 from walker import *
 import numpy as np
 
-ps = perm_screen(1000, 1000, perm(triangular_walker(np.array([26, 16]), self_avoiding=True, scaling_factor=1.85),100, 10000), update_steps=1, scale = 20, sleep_time=.0, save = True)
+ps = perm_screen(1000, 1000, perm(hexagonal_walker(np.array([26, 24]), self_avoiding=True, scaling_factor=4.2),100, 10000), update_steps=1, scale = 20, sleep_time=.0, save = True)
 

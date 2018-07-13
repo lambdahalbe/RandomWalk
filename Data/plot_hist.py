4d421743ea8b502e1c5c8a9cc44a1b05b6c42e92
Fabian Glatzel <fabian.glatzel@googlemail.com>
2018-07-13 12:41:32 +0200

    added fits to plots of hists

--- Data/plot_hist.py
+++ Data/plot_hist.py
@@ -1,5 +1,7 @@
 import matplotlib.pylab as plt
 import numpy as np
+from scipy.optimize import curve_fit
+
 
 data = {}
 keys = [10, 100, 1000]
@@ -19,11 +21,26 @@ for i in keys:
     data[i][0] = np.array(data[i][0])
     data[i][1] = np.array(data[i][1])
 
+def gaussian(x, mu, sigma, A):
+    return A * np.exp(-(x - mu)**2 / (2 * sigma**2))
+
 
 bins = [5, 50, 500]
 for i in range(3):
 
-    plt.hist(data[keys[i]][0], bins=bins[i], weights=data[keys[i]][0], density=True)
+    y, x, patches = plt.hist(data[keys[i]][0], bins=bins[i], weights=data[keys[i]][0], density=True)
+    for j in range(len(x) - 1):
+        x[i] = 0.5 * (x[j] + x[j + 1])
+    x = x[:-1]
+    A0 = max(y)
+    mu0 = x[-1] / 2
+    sigma0 = mu0 / 2
+    results = curve_fit(gaussian, x, y, p0 = (mu0, sigma0, A0))
+    print("mu", results[0][0])
+    print("sigma", results[0][1])
+    x = np.linspace(min(x), max(x), 1000)
+    if i in [1, 2]:
+        plt.plot(x, gaussian(x, *results[0]), "--")
     plt.xlabel("End-To-End-Distance")
     plt.ylabel("Probability")
     plt.show()
